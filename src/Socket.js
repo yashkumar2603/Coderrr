1109ef0e8698d87f904e67763988d294ba33ea31
yashkumar2603 <yash.2603kumar@gmail.com>
2024-06-24 20:41:53 +0530

    Changes to handle SSL certificates issues

--- src/Socket.js
+++ src/Socket.js
@@ -1,11 +1,20 @@
-import {io} from 'socket.io-client';
+import { io } from 'socket.io-client';
 
-export const initSocket = async () =>{
+export const initSocket = async () => {
     const options = {
         'force new connection': true,
-        reconnectionAttempts : 'Infinity',
+        reconnectionAttempts: 'Infinity',
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io(process.env.REACT_APP_BACKEND_URL, options);
-}
+
+    // Check if backend URL starts with http and convert to ws/wss accordingly
+    let backendUrl = process.env.REACT_APP_BACKEND_URL;
+    if (backendUrl.startsWith('https')) {
+        backendUrl = backendUrl.replace('https', 'wss');
+    } else if (backendUrl.startsWith('http')) {
+        backendUrl = backendUrl.replace('http', 'ws');
+    }
+
+    return io(backendUrl, options);
+};
